8c2963bc6f0e1a05d7900e69e5b25f9cd07116f0
Sonic803 <sonic82003@gmail.com>
2022-07-04 22:32:19 +0200

    meglio

--- info.py
+++ info.py
@@ -7,15 +7,37 @@ import requests
 from time import sleep
 api = mangadex.Api()
 
+#Download url to path if not exists
+def download(url,path,force=False):
+    if not os.path.exists(path) or force:
+        img_data = requests.get(url).content
+        with open(path, 'wb') as handler:
+            handler.write(img_data)
+        return 1
+    return 0
+
+def downloadCover(covers,volume,path):
+    if volume in covers:
+        url = covers[volume].fetch_cover_image()
+        createDir(path)
+        download(url,f"{path}/cover.jpg",force=True)
+        return 1
+    return 0
+
+def createDir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        return 1
+    return 0
 
 class Manga:
     def __init__(self, idManga,language='en'):
         self.language=language
-        self.quant=3
         self.idManga=idManga
         self.manga=api.view_manga_by_id(manga_id=self.idManga)
+
         self.volumes=api.get_manga_volumes_and_chapters(
-            manga_id=idManga, limit=1000,translatedLanguage=self.language)
+            manga_id=idManga, limit=300,translatedLanguage=self.language) #API limita a 300 volumi
         self.coverId=self.manga.coverId
         self.title=list(self.manga.title.values())[0]
         self.description=self.manga.description
@@ -44,41 +66,16 @@ class Manga:
             else:
                 self.groupsNum[a] = 1
         print(self.groupsNum)
-        #get the largest element from groupsNum
-
-
-        # self.volumes=self.manga.volumes
-        # self.last_chapter=self.manga.last_chapter
-        # self.last_volume=self.manga.last_volume
-        # self.views=self.manga.views
-        # self.favorites=self.manga.favorites
-        # self.date_uploaded=self.manga.date_uploaded
-        # self.date_last_updated=self.manga.date_last_updated
-        # self.date_last_chapter_updated=self.manga.date_last_chapter_updated
-        # self.is_manga=self.manga.is_manga
-        # self.is_licenced=self.manga.is_licenced
-        # self.is_adult=self.manga.is_adult
-        # self.is_completed=self.manga.is_completed
-        # self.is_ongoing=self.manga.is_ongoing
-        # self.is_licensed=self.manga.is_licensed
-        # self.is_hentai=self.manga.is_hentai
-        # self.is_mature=self.manga.is_mature
-        # self.is_partial=self.manga.is_partial
-        # self.is_nsfw=self.manga.is_nsfw
-        # self.is_liked=self.manga.is_liked
-        # self.is_disliked=self
 
     def __str__(self):
-        # return f"{self.manga}"
-        return f"{self.volumes}"
+        return f"{self.manga}"
+        #return f"{self.volumes}"
 
-    def download(self):
+    def save(self):
         chapt=[]
         for i in range (10):
             chapt.extend(api.chapter_list(manga=self.idManga,translatedLanguage=self.language,limit=100,offset=100*i))
-        for a in chapt:
-            print(a.volume,a.chapter)
-        print(self.groupsNum)
+
         vett={}
         for a in chapt:
             indice=a.chapter
@@ -109,42 +106,28 @@ class Manga:
 
             volume=a.volume
             path = f'./{self.title}/{volume}'
-            if not os.path.exists(path):
-                os.makedirs(path)
+            createDir(path)
+            coverPath=f"{path}/\"0cover"
+            downloadCover(covers,volume,coverPath)
 
-
-            if not os.path.exists(f"{path}/0cover"):
-                os.makedirs(f"{path}/0cover")
-                print(covers[volume].fetch_cover_image())
-                img_data = requests.get(covers[volume].fetch_cover_image()).content
-                with open(f'{path}/0cover/{volume}.jpg', 'wb') as handler:
-                    handler.write(img_data)
 
             path=path+f"/{a.chapter}"
-            if not os.path.exists(path):
-                os.makedirs(path)
+            createDir(path)
+
             links=a.fetch_chapter_images()
-            #print(links)
-            print(a.chapter)
+
+            #for i,link in enumerate(links):
+            #    print(link)
+            #    download(link,f"{path}/{i}.jpg")
             
-            for i,b in enumerate(links):
-                if not os.path.exists(f'{path}/{i}.jpg'):
-                    img_data = requests.get(b).content
-                    with open(f'{path}/{i}.jpg', 'wb') as handler:
-                        handler.write(img_data)
-                else:
-                    print(f"Esiste: {a.chapter}.{i}")
-            #fetch_cover_image
+            
             sleep(0.5)
-
-
-        return 42
     
 
 if __name__ == "__main__":
-    idManga='d86cf65b-5f6c-437d-a0af-19a31f94ec55'
+    idManga='a1422bad-4598-4018-95ae-888435bafe67'
     manga=Manga(idManga)
     # print(manga)
     #print(manga.capitoli)
     print(manga.volumes)
-    manga.download()
+    manga.save()
