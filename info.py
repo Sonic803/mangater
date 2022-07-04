fca128ef22c515efc9c49aa4daac794631cb044d
Sonic803 <sonic82003@gmail.com>
2022-07-04 23:40:00 +0200

    Meglio

--- info.py
+++ info.py
@@ -14,23 +14,37 @@ from concurrent.futures import as_completed
 
 api = mangadex.Api()
 
-#Download url to path if not exists
-def download(url,path,force=False):
+# Download url to path if not exists
+
+
+def download(url, path, force=False):
     if not os.path.exists(path) or force:
         img_data = requests.get(url).content
         with open(path, 'wb') as handler:
             handler.write(img_data)
         return 1
     return 0
-def download_for_multiple(url,path,force=False):
-    return (download(url,path,force),path)
 
-def download_multiple(urls_paths,force=False):
+
+def getcovers(coverss, extension='.jpg'):
+    covers={}
+    for a in coverss:
+        if a.fileName.find(extension)!=-1:
+            covers[a.volume]=a
+    return covers
+
+
+def download_for_multiple(url, path, force=False):
+    return (download(url, path, force), path)
+
+
+def download_multiple(urls_paths, force=False):
     with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
-        futures = [executor.submit(download_for_multiple, url_path[0], url_path[1]) for url_path in urls_paths]
+        futures = [executor.submit(
+            download_for_multiple, url_path[0], url_path[1]) for url_path in urls_paths]
         for future in as_completed(futures):
             # retrieve result
-            res,path  = future.result()
+            res, path = future.result()
             # check for a link that was skipped
             """if res :
                 print(f'Downloaded')
@@ -38,13 +52,14 @@ def download_multiple(urls_paths,force=False):
                 print(f'>skipped {path}')"""
 
 
-def downloadCover(covers,volume,path):
+def downloadCover(covers, volume, path, CoverFormat):
     if volume in covers:
         url = covers[volume].fetch_cover_image()
         createDir(path)
-        download(url,f"{path}/cover.jpg",force=True)
+        download(url, f"{path}/cover{CoverFormat}", force=True)
         return 1
     return 0
+
 
 def createDir(path):
     if not os.path.exists(path):
@@ -52,15 +67,17 @@ def createDir(path):
         return 1
     return 0
 
+
 class Manga:
-    def __init__(self, idManga,language='en'):
+    def __init__(self, idManga, language='en', CoverFormat='jpg'):
         self.language=language
         self.idManga=idManga
         self.manga=api.view_manga_by_id(manga_id=self.idManga)
 
         self.volumes=api.get_manga_volumes_and_chapters(
-            manga_id=idManga, limit=300,translatedLanguage=self.language) #API limita a 300 volumi
+            manga_id=idManga, limit=300, translatedLanguage=self.language)  # API limita a 300 volumi
         self.coverId=self.manga.coverId
+        self.CoverFormat=CoverFormat
         self.title=list(self.manga.title.values())[0]
         self.description=self.manga.description
         self.authorId=self.manga.authorId
@@ -69,15 +86,15 @@ class Manga:
         self.status=self.manga.status
         self.year=self.manga.year
         self.chapters=[]
-        for i in range (10):
-            self.chapters.extend(api.manga_feed(manga_id=idManga,limit=100,offset=100*i,translatedLanguage=self.language))
-        
+        for i in range(10):
+            self.chapters.extend(api.manga_feed(
+                manga_id=idManga, limit=100, offset=100 *i, translatedLanguage=self.language))
+
         # self.chapters=api.chapter_list(manga=idManga,limit=3)
 
         self.volumi=list(self.volumes.values())
         self.capitoli=[list(a.values())[0] for a in list(self.volumi)]
         capitoli=[a['chapters'] for a in self.volumi]
-
 
         self.groups=[a.scanlation_group_id for a in self.chapters]
 
@@ -90,12 +107,18 @@ class Manga:
 
     def __str__(self):
         return f"{self.manga}"
-        #return f"{self.volumes}"
+        # return f"{self.volumes}"
 
     def save(self):
+        k=0
         chapt=[]
-        for i in range (10):
-            chapt.extend(api.chapter_list(manga=self.idManga,translatedLanguage=self.language,limit=100,offset=100*i))
+        cha=api.chapter_list(
+                manga=self.idManga, translatedLanguage=self.language, limit=100, offset=100 *k)
+        while len(cha)>0:
+            chapt.extend(cha)
+            k=k+1
+            cha=api.chapter_list(
+                manga=self.idManga, translatedLanguage=self.language, limit=100, offset=100 *k)
 
         vett={}
         for a in chapt:
@@ -113,39 +136,31 @@ class Manga:
         for a in e:
             f.append(vett[a])
 
-        coverss=api.get_coverart_list(manga=self.idManga,limit=100)
-        covers={}
-        for a in coverss:
-            if   a.fileName.find('.jpg')!=-1:
-                covers[a.volume]=a
-        URLS_PATHS_TOT=[]
-        conta=1
-        for i,a in enumerate(f):
-            
-            volume=a.volume
+        coverss=api.get_coverart_list(manga=self.idManga, limit=100)
+        covers=getcovers(coverss, self.CoverFormat)
+
+        for i, mangach in enumerate(f):
+
+            volume=mangach.volume
             path = f'./{self.title}/{volume}'
             createDir(path)
             coverPath=f"{path}/\"0cover"
-            downloadCover(covers,volume,coverPath)
+            downloadCover(covers, volume, coverPath, self.CoverFormat)
 
-            chapter=a.chapter
-            path=path+f"/{chapter}"
+            chapter=mangach.chapter
+            path=path +f"/{chapter}"
             createDir(path)
-            loading(i/len(f),f'Downloading {volume}-{chapter}')
-            links=a.fetch_chapter_images()
-            URLS_PATHS=[[link,f"{path}/{i}.jpg"] for i,link in enumerate(links)]
-            URLS_PATHS_TOT.extend(URLS_PATHS)
 
-            if not conta:
-                download_multiple(URLS_PATHS_TOT)
-                
-                URLS_PATHS_TOT=[]
+            loading(i /len(f), f'Downloading {volume}-{chapter}')
 
-            conta=(conta+1)%2
-    
+            links=mangach.fetch_chapter_images()
+            URLS_PATHS=[[link, f"{path}/{i}.jpg"]
+                        for i, link in enumerate(links)]
+            download_multiple(URLS_PATHS)
+
 
 if __name__ == "__main__":
     idManga='d86cf65b-5f6c-437d-a0af-19a31f94ec55'
-    manga=Manga(idManga,'it')
-    print(manga)
+    manga=Manga(idManga, 'en', '.png')
+    print(manga.title)
     manga.save()
